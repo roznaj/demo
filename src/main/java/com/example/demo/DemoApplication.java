a428ad35de0d8860091c8bb796bf5b7c2f758755
roznaj <bassahrozlyn@gmail.com>
2020-11-20 16:57:32 +0100

    Spring Boot - Integrating Static Content - Javascript (JS) and CSS files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer implements WebMvcConfigurer{
+   
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+       
+        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+        
+    }
 
 }
